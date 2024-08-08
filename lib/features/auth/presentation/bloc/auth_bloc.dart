7bb70e093d78f9c986d7dd612fca5e1c6ea1b7e0
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-08-08 22:54:47 +0530

    Adding signup bloc

---
+++ lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:bloggers_hub/core/error/failure.dart';
+import 'package:bloggers_hub/features/auth/domain/usecases/user_signup.dart';
+import 'package:meta/meta.dart';
+import 'package:fpdart/fpdart.dart';
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  final UserSignup _userSignup;
+  AuthBloc({required UserSignup userSignup})
+      : _userSignup = userSignup,
+        super(AuthInitial()) {
+    on<AuthSignUp>((event, emit) async {
+      emit(AuthLoading());
+      final result = await _userSignup(UserSignupParams(
+          email: event.email, password: event.password, name: event.name));
+      result.fold((failure) {
+        emit(AuthFailure(failure.message));
+      }, (uid) {
+        emit(AuthSuccess(uid));
+      });
+    });
+  }
+}
